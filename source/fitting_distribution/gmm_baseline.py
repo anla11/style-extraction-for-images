b7f6dc0a0a3ca653550a345b4cf5570916796d02
anvy1102 <langocthuyan@gmail.com>
2017-10-03 16:26:27 +0700

    gmm baseline

--- source/fitting_distribution/gmm_baseline.py
+++ source/fitting_distribution/gmm_baseline.py
@@ -3,18 +3,11 @@ import pandas as pd
 from scipy.stats import multivariate_normal
 from sklearn.mixture import GaussianMixture
 from sklearn.model_selection import StratifiedKFold
-
-
 import pickle
-usr_list = None
-with open ('usr_list.txt', 'rb') as fp:
-    usr_list = pickle.load(fp) 
-len(usr_list)
 
-data = np.loadtxt('data.txt')
+import sys
+sys.path.append(".")
 
-item_list = np.loadtxt('item_list.txt').astype(int)
-len(item_list)
 
 
 class  Cluster:
@@ -40,13 +33,13 @@ def cal_ptrans_item_user(item, user, features, clusters):
     return np.sum([cal_logp_item_cluster(features[item], cluster) + cal_logp_user_cluster(user, features, cluster)\
                    for cluster in clusters])
     
-def  find_clusters_baseline(items, features, n_cluster):
+def  find_clusters(items, features, n_cluster):
     '''
         itemset: list indexes of images corresponding to video_img. Discard indexes where feature is None.
         usr_list: |U| users, each user is a sets of items.
         feature: gist or lab feature coressponding image in video_img.
     '''
-    label_init = np.random.randint(0, n_cluster, len(item))
+    label_init = np.random.randint(0, n_cluster, len(items))
 
     gmm = GaussianMixture(n_components=n_cluster, covariance_type = 'full', max_iter = 100, random_state=0)
     gmm.means_init = np.array([features[items[label_init == i]].mean(axis=0) for i in range(n_cluster)])
@@ -64,3 +57,19 @@ def  validate(gmm, usr_list, features):
         p_users.append(p_user)
         print p_user
     return np.array(p_users).mean()
+
+
+usr_list = None
+with open ('../../data/usr_his_sample/usr_list_5-20.txt', 'rb') as fp:
+    usr_list = pickle.load(fp) 
+len(usr_list)
+
+features = np.loadtxt('../../data/usr_his_sample/features.txt')
+
+item_list = np.loadtxt('../../data/usr_his_sample/item_list.txt').astype(int)
+
+gmm = find_clusters(item_list, features, n_cluster=32)
+
+means, covs, weights = gmm.means_, gmm.covariances_, gmm.weights_
+clusters = [Cluster(means[i], covs[i], weights[i]) for i in range(len(means))]
+
